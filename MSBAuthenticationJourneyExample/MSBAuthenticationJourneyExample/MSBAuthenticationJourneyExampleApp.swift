5a0c04c13a2bafc7aa357681c2bb064feda5486a
Dat Doan <datdv1@smartosc.com>
2024-11-11 12:13:30 +0700

    Init MSBAuthenticationJourney

---
+++ MSBAuthenticationJourneyExample/MSBAuthenticationJourneyExample/MSBAuthenticationJourneyExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSBAuthenticationJourneyExampleApp.swift
+//  MSBAuthenticationJourneyExample
+//
+//  Created by doandat on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MSBAuthenticationJourneyExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MSBAuthenticationJourneyExample/MSBAuthenticationJourneyExampleTests/MSBAuthenticationJourneyExampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  MSBAuthenticationJourneyExampleTests.swift
+//  MSBAuthenticationJourneyExampleTests
+//
+//  Created by doandat on 11/11/24.
+//
+
+import Testing
+
+struct MSBAuthenticationJourneyExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
